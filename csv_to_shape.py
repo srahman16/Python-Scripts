7610d53f9a4e2f598d7d44919076284e7c28557b
Shahriar Rahman <rahman.shahriar@mail.com>
2023-01-28 22:28:32 +1100

    Update csv_to_shape.py

--- csv_to_shape.py
+++ csv_to_shape.py
@@ -1,3 +1,9 @@
+'''
+Created by: Shahriar Rahman
+Email: shahriar.env12@gmail.com
+Description: This script will convert all the latitude and longitude (in WGS84 & in Degree Decimal) from a comma-separted value (.csv) file to a shapefile (.shp)
+'''
+
 import arcpy
 
 # Set the workspace
@@ -12,3 +18,6 @@ output_shp = "POI_AUS.shp"
 # Use the XY Table to Point tool to create a point shapefile
 arcpy.management.XYTableToPoint(csv_file, output_shp, "Long", "Lat", "", "")
 print("Well done! Shapefile created.")
+#End of script#
+## Please subscribe to my youtube channel ##
+############################################
